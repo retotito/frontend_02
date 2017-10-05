afbda66a47193a473df315f0e83eba216da031f2
retotito <retotito@gmail.com>
2017-10-05 12:20:18 +0200

    mobile menu

--- src/app/header/header-mobile/header-mobile.component.ts
+++ src/app/header/header-mobile/header-mobile.component.ts
@@ -4,7 +4,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header-mobile',
   templateUrl: './header-mobile.component.html',
-  styleUrls: ['./header-mobile.component.css']
+  styleUrls: ['./header-mobile.component.css'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
 export class HeaderMobileComponent implements OnInit {
 
@@ -13,7 +16,12 @@ export class HeaderMobileComponent implements OnInit {
 
   triggerMobileMenu () {
     this.mmService.isOpen = !this.mmService.isOpen;
-    console.log("MMservice: " + this.mmService.isOpen);
+    this.mmService.callComponentMethod();
+  }
+
+  onResize(event){
+    this.mmService.isOpen = false;
+    this.mmService.callComponentMethod();
   }
 
   ngOnInit() {
--- src/app/header/header-mobile/mobilemenu/mobilemenu.component.ts
+++ src/app/header/header-mobile/mobilemenu/mobilemenu.component.ts
@@ -1,15 +1,51 @@
+import { MobileMenuService } from './../../../services/mobile-menu.service';
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-mobilemenu',
   templateUrl: './mobilemenu.component.html',
-  styleUrls: ['./mobilemenu.component.css']
+  styleUrls: ['./mobilemenu.component.css'],
+  animations: [
+    trigger('menuState', [
+      state('open', style({
+        transform: 'translateX(-100%)',
+        display: 'block'
+      })),
+      state('closed',   style({
+        transform: 'translateX(0px)',
+        display: 'none'
+      })),
+      transition('closed <=> open', animate('200ms ease-in')),
+    ])
+  ]
 })
-export class MobilemenuComponent implements OnInit {
+export class MobilemenuComponent  {
+  myMenuState = 'closed';
 
-  constructor() { }
+  constructor(private mmService: MobileMenuService) { }
 
-  ngOnInit() {
+  myServiceListener = this.mmService.componentMethodCalled$.subscribe(
+    (isOpen) => {
+      if (isOpen) { 
+        this.myMenuState = 'open';
+      } else
+      { 
+        this.myMenuState = 'closed';
+      }
+    }
+  );
+
+  closeMenu () {
+    this.myMenuState = 'closed';
+    this.mmService.isOpen = false;
   }
+  
 
 }
