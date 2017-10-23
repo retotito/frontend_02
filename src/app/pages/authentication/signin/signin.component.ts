55baafc4abea82d7099d9e691c01cbeed171d3c2
retotito <retotito@gmail.com>
2017-10-23 16:50:41 +0200

    hom page

--- src/app/pages/authentication/signin/signin.component.ts
+++ src/app/pages/authentication/signin/signin.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
 
--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -3,9 +3,56 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-home',
   templateUrl: './page-home.component.html',
-  styleUrls: ['./page-home.component.css']
+  styleUrls: ['./page-home.component.scss']
 })
 export class PageHomeComponent implements OnInit {
+  logos = [
+    { title: 'Brick 2',
+      src: 'assets/images/logo-angular.png'},
+    { title: 'Brick 2',
+      src: 'assets/images/logo-nodejs.png'},
+    { title: 'Brick 2',
+      src: 'assets/images/logo-mongodb.png'},
+    { title: 'Brick 2',
+      src: 'assets/images/logo-express.png'},
+    { title: 'Brick 2',
+      src: 'assets/images/logo-typescript.png'},
+    { title: 'Brick 2',
+      src: 'assets/images/logo-sass.png'},
+    { title: 'Brick 2',
+      src: 'assets/images/logo-ubuntu.png'},
+    { title: 'Brick 2',
+      src: 'assets/images/logo-elastic.png'},
+    { title: 'Brick 2',
+      src: 'assets/images/logo-html.png'},
+    { title: 'Brick 2',
+      src: 'assets/images/logo-css3.png'}
+ ]
+
+ todos = [
+    { name:"01",
+      status:"done"},
+    { name:"02",
+      status:"done"},
+      { name:"03",
+      status:"done"},
+      { name:"04",
+      status:"done"},
+      { name:"05",
+      status:"open"},
+      { name:"06",
+      status:"open"},
+      { name:"07",
+      status:"open"},
+      { name:"08",
+      status:"open"},
+      { name:"09",
+      status:"open"},
+      { name:"10",
+      status:"open"},
+      { name:"11",
+      status:"open"},
+ ]
 
   user = {
     name: 'Reto',
